b9d4e30a528ad28b49b7288f6904c33947258aaf
Sandra Lozano Sanchez <sandra.lozanosanchez@plexus.local>
2022-08-03 06:27:52 +0200

    sumatorio init

---
+++ src/main/java/com/opplus/sumatorio/SumatorioApplication.java
@@ -0,0 +1,13 @@
+package com.opplus.sumatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumatorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opplus/sumatorio/SumatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opplus.sumatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
